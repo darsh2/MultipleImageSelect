4eddcec850e9135379ccd012a389353d9e5064fb
darsh2 <darsh.d2@gmail.com>
2015-05-26 15:04:49 +0530

    Constants

---
+++ multipleimageselect/src/main/java/com/darsh/multipleimageselect/helpers/Constants.java
@@ -0,0 +1,18 @@
+package com.darsh.multipleimageselect.helpers;
+
+/**
+ * Created by Darshan on 5/26/2015.
+ */
+public class Constants {
+    public static final int REQUEST_CODE = 2000;
+
+    public static final String INTENT_EXTRA_IMAGES = "images";
+    public static final String INTENT_EXTRA_LIMIT = "limit";
+    public static final int DEFAULT_LIMIT = 10;
+
+    //Maximum number of images that can be selected at a time
+    public static int limit;
+
+    //String to display when number of selected images limit is exceeded
+    public static String toastDisplayLimitExceed = String.format("Can select maximum of %d images", limit);
+}
